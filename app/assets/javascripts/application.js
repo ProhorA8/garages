9d749ec4b9776f5fb35981445c8212e5acb1ccdb
ProhorA8 <Procustom4@gmail.com>
2019-08-07 22:34:27 +0300

    adminlte config

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
-//= require turbolinks
-//= require app
 //= require cable
+//= require jquery3
+//= require jquery-ui
+//= require jquery_ujs
+//= require popper
+//= require bootstrap-sprockets
+//= require app
