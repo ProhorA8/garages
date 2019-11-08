0788109f691d82281581b5677d21b7344e3952b1
ProhorA8 <Procustom4@gmail.com>
2019-11-09 02:21:59 +0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Users.destroy_all
-
-Users.create ([])
+User.create!(email: 'Hello@gmail.com',
+             password: '123456789',
+             password_confirmation: '123456789')
